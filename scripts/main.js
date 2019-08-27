810a6ebe6d2ef3c2117beb502f721059be9fd374
Jerry Li <xiaoroyg@163.com>
2019-08-27 22:26:00 +0800

    Learned the javascript basics.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,12 @@
-let myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+// let myHeading = document.querySelector('h1');
+// myHeading.textContent = 'Hello world!';
+let heading = document.querySelector('h1');
+if(!localStorage.getItem('name')) {
+    setUserName()
+} else {
+    let storedName = localStorage.getItem('name');
+    heading.textContent = 'Mozilla is cool, ' + storedName;
+}
 
 let image = document.querySelector('img');
 image.onclick = function() {
@@ -8,4 +15,15 @@ image.onclick = function() {
     let secondSrc = 'images/iron-man.jpeg';
     let result = (src === firstSrc ? secondSrc : firstSrc);
     image.setAttribute('src', result);
+}
+
+let buttonChangeUser = document.querySelector('button');
+buttonChangeUser.onclick = function() {
+    setUserName();
+}
+
+function setUserName() {
+    let name = prompt('Please enter your name');
+    localStorage.setItem('name', name);
+    heading.textContent = 'Mozilla is cool, ' + name;
 }
