524c681fa4aace8febe340561eeed017a4a8ba60
Jerry Li <xiaoroyg@163.com>
2019-08-31 21:36:26 +0800

    Finished the number guessing game.

--- learnning-area/javascirpt/introduction-to-js/first-spalsh/guess.js
+++ learnning-area/javascirpt/introduction-to-js/first-spalsh/guess.js
@@ -1,6 +1,6 @@
-let randomNumber = Math.floor(Math.random() * 100) + 1;
+let randomNumber = createRandomNumber();
 
-const guesses = document.querySelector('.guess');
+const guesses = document.querySelector('.guesses');
 const lastResult = document.querySelector('.lastResult');
 const lowOrHigh = document.querySelector('.lowOrHigh');
 
@@ -10,18 +10,23 @@ const guessField = document.querySelector('.guessField');
 let guessCount = 1;
 let resetButton;
 
+function createRandomNumber() {
+    return Math.floor(Math.random() * 100) + 1;
+    // return 44;
+}
+
 function checkGuess() {
     let userGuess = Number(guessField.value);
     if(guessCount === 1) {
         guesses.textContent = 'Previous guesses: ';
     }
-    guesses.textContent += userGuess + '';
+    guesses.textContent += userGuess + ' ';
     if(userGuess === randomNumber) {
         lastResult.textContent = 'Congratulations! You got it right!';
         lastResult.style.backgroundColor = 'green';
         lowOrHigh.textContent = '';
         setGameOver();
-    } else if (guessCount == 10) {
+    } else if (guessCount == 5) {
         lastResult.textContent = '!!!GAME OVER!!!';
         setGameOver();
     } else {
@@ -39,6 +44,31 @@ function checkGuess() {
     guessField.focus();
 }
 
-function setGameOver() {
+guessSubmit.addEventListener('click', checkGuess);
 
+function setGameOver() {
+    guessField.disabled = true;
+    guessSubmit.disabled = true;
+    resetButton = document.createElement('button');
+    resetButton.textContent = 'Start new game.';
+    document.body.appendChild(resetButton);
+    resetButton.addEventListener('click', resetGame);
+}
+
+function resetGame() {
+    guessCount = 1;
+
+    const resetParas = document.querySelectorAll('.resultParas p');
+    for(let index = 0; index < resetParas.length; index ++) {
+        resetParas[index] = textContent = '';
+    }
+    resetButton.parentNode.removeChild(resetButton);
+    guessField.disabled = false;
+    guessSubmit.disabled = false;
+    guessField.value = '';
+    guessField.focus();
+
+    lastResult.style.backgroundColor = 'white';
+
+    randomNumber = createRandomNumber();
 }
