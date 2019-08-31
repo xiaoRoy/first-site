4ff38ac693177a50f1de2364229809bdefcd4749
Jerry Li <xiaoroyg@163.com>
2019-08-31 11:35:17 +0800

    Finished the check guess.

--- learnning-area/javascirpt/introduction-to-js/first-spalsh/guess.js
+++ learnning-area/javascirpt/introduction-to-js/first-spalsh/guess.js
@@ -11,5 +11,34 @@ let guessCount = 1;
 let resetButton;
 
 function checkGuess() {
-    alert('I am a placeholder');
+    let userGuess = Number(guessField.value);
+    if(guessCount === 1) {
+        guesses.textContent = 'Previous guesses: ';
+    }
+    guesses.textContent += userGuess + '';
+    if(userGuess === randomNumber) {
+        lastResult.textContent = 'Congratulations! You got it right!';
+        lastResult.style.backgroundColor = 'green';
+        lowOrHigh.textContent = '';
+        setGameOver();
+    } else if (guessCount == 10) {
+        lastResult.textContent = '!!!GAME OVER!!!';
+        setGameOver();
+    } else {
+        lastResult.textContent = 'Wrong!';
+        lastResult.style.backgroundColor = 'red';
+        if(userGuess < randomNumber) {
+            lowOrHigh.textContent = "Last guest was too low!";
+        } else if(userGuess > randomNumber){
+            lowOrHigh.textContent = "Last guest was too high!";
+        }
+    }
+
+    guessCount ++;
+    guessField.value = '';
+    guessField.focus();
+}
+
+function setGameOver() {
+
 }
