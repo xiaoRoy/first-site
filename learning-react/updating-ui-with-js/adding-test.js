6067bdcf622f12f5e7b753ee3487f604a69367e1
Jerry Li <53632783+jerli4@users.noreply.github.com>
2023-11-18 15:08:02 +0800

    Learned react - first glimpse of react.

---
+++ learning-react/updating-ui-with-js/adding-test.js
@@ -0,0 +1,8 @@
+const app = document.getElementById('app');
+
+const header = document.createElement('h1');
+
+const text = 'Develop. Preview. Ship. 🚀';
+const headerContent = document.createTextNode(text);
+header.appendChild(headerContent);
+app.appendChild(header)
