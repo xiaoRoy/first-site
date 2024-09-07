91f56cae5af5a3829609cdbea10c68fa3fb4f745
xiaoRoy <xiaoroyg@163.com>
2024-09-07 23:58:30 +0800

    Reformat the code.

--- src/learning-react/updating-ui-with-js/adding-test.js
+++ src/learning-react/updating-ui-with-js/adding-test.js
@@ -1,8 +1,8 @@
-const app = document.getElementById('app');
+const app = document.getElementById("app");
 
-const header = document.createElement('h1');
+const header = document.createElement("h1");
 
-const text = 'Develop. Preview. Ship. 🚀';
+const text = "Develop. Preview. Ship. 🚀";
 const headerContent = document.createTextNode(text);
 header.appendChild(headerContent);
-app.appendChild(header)
+app.appendChild(header);
