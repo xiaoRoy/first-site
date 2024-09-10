6747bfaced159cd06bdb9c43e19065652abc56eb
xiaoRoy <xiaoroyg@163.com>
2024-09-10 23:36:58 +0800

    Learned simple nesting.

--- src/learning-react/zero/scripts/header.js
+++ src/learning-react/zero/scripts/header.js
@@ -1,3 +1,4 @@
-const helloWorld = React.createElement("h1", null, "Hello World!!!");
+const world = React.createElement("em", null, "world");
+const helloWorld = React.createElement("h1", null, "Hello ", world, "!");
 const reactRoot = ReactDOM.createRoot(document.getElementById("root"));
 reactRoot.render(helloWorld);
