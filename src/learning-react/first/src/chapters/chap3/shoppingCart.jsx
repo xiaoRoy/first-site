74f0d3017d75a09adb5d7de7d015448c16bf8bed
xiaoRoy <xiaoroyg@163.com>
2024-09-14 22:59:06 +0800

    Learned the allocation of logic to components

--- src/learning-react/first/src/chapters/chap3/shoppingCart.jsx
+++ src/learning-react/first/src/chapters/chap3/shoppingCart.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 
 class ShoppingCart extends Component {
   render() {
@@ -19,5 +19,50 @@ class ShoppingCart extends Component {
 class CardItems extends Component {
   render() {
     return <div></div>;
+  }
+}
+
+class GuestButton extends Component {
+  render() {
+    const canCheckout = this.props.canCheckout;
+    return (
+      <Fragment>
+        <button>Login</button>
+        <button disabled={canCheckout}>Checkout as guest</button>
+      </Fragment>
+    );
+  }
+}
+
+class UserButton extends Component {
+  render() {
+    const { canCheckout, hasCreditCard, hasAddress } = this.props.userStatus;
+    return hasCreditCard ? (
+      <Fragment>
+        <button disabled={canCheckout}>Checkout</button>
+        {hasAddress && <button disabled={canCheckout}>One click buy</button>}
+      </Fragment>
+    ) : (
+      <button>Add credit card</button>
+    );
+  }
+}
+
+class ShoppingCartV2 extends Component {
+  render() {
+    //under mvvm, these logics should go into view model;
+    const isLoggedIn = this.props.users !== null;
+    const items = this.props.items;
+    const canCheckout =
+      items.length > 0 && items.every((item) => !item.outOfStock);
+    const hasCreditCard = this.props.user.creditCard !== null;
+    const hasAddress = this.props.user.address !== null;
+    const userStatus = { canCheckout, hasCreditCard, hasAddress };
+
+    return isLoggedIn ? (
+      <UserButton userStatus={userStatus}></UserButton>
+    ) : (
+      <GuestButton canCheckout={canCheckout}></GuestButton>
+    );
   }
 }
