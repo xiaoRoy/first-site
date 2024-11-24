3cd9c43c4edb272e1d911ade87755e23d8aeea8e
xiaoRoy <xiaoroyg@163.com>
2024-11-24 23:18:18 +0800

    Finished the status form.

--- src/learning-react/first/src/react-official/manage-state/statusFrom.jsx
+++ src/learning-react/first/src/react-official/manage-state/statusFrom.jsx
@@ -1,13 +1,19 @@
+import { useState } from "react";
 import "./styles/status-form.css";
 
 const formStatus = {
   empty: "empty",
+  typing: "typing",
   success: "success",
   submitting: "submitting",
   error: "error",
 
   isEmpty: function (status) {
     return this.empty === status;
+  },
+
+  isTyping: function (status) {
+    return this.typing === status;
   },
 
   isSuccessful: function (status) {
@@ -27,22 +33,58 @@ function SuccessStatusForm() {
   return <h1>That's right!</h1>;
 }
 
-function BaseStatusForm({ status }) {
+class WrongAnswer extends Error {
+  constructor(message) {
+    super(message);
+  }
+}
+
+async function submitForm(answer) {
+  await new Promise((resolve) => setTimeout(resolve, 1500));
+  const isWrongAnswer = "lima" !== answer;
+  if (isWrongAnswer) {
+    throw new WrongAnswer("Good guess but a wrong answer. Try again!");
+  }
+}
+
+function BaseStatusForm({ status, setCurrentStatus }) {
+  const [answer, setAnswer] = useState("");
+  const [error, setError] = useState(null);
   const isAnswerDisabled = formStatus.isSubmitting(status);
   const isSubmitDisabled =
-    formStatus.isEmpty(status) || formStatus.isSubmitting(status);
-  const isErrorMessageVisible = formStatus.isError(status);
+    formStatus.isEmpty(status) || formStatus.isSubmitting(status) || answer.length === 0;
+  const isErrorMessageVisible = formStatus.isError(status) || error != null;
+
+  const onAnswerInputting = (event) => setAnswer(event.target.value);
+
+  const onSubmitted = async (event) => {
+    event.preventDefault();
+    setCurrentStatus(formStatus.submitting);
+    try {
+      await submitForm(answer);
+      setCurrentStatus(formStatus.success);
+    } catch (error) {
+      if (error instanceof WrongAnswer) {
+        setCurrentStatus(formStatus.typing);
+        setError(error);
+      } else {
+        throw error;
+      }
+    }
+  };
   return (
     <>
       <h2>City quiz</h2>
       <p>
         In which city is there a billboard that turns air into drinkable water?
       </p>
-      <form action="" className="state-form">
+      <form action="" className="state-form" onSubmit={onSubmitted}>
         <textarea
           name="quiz-answer"
           id=""
           disabled={isAnswerDisabled}
+          onChange={onAnswerInputting}
+          value={answer}
         ></textarea>
         <button disabled={isSubmitDisabled} className="button-submit">
           submit
@@ -55,16 +97,21 @@ function BaseStatusForm({ status }) {
   );
 }
 
-function StatusForm({ status = formStatus.empty }) {
-  console.log(`Status Form:${status}`);
+function StatusForm({ status = formStatus.typing }) {
+  const [currentStatus, setCurrentStatus] = useState(status);
   let form;
-  switch (status) {
+  switch (currentStatus) {
     case formStatus.success:
       form = <SuccessStatusForm></SuccessStatusForm>;
       break;
 
     default:
-      form = <BaseStatusForm status={status}></BaseStatusForm>;
+      form = (
+        <BaseStatusForm
+          status={currentStatus}
+          setCurrentStatus={setCurrentStatus}
+        ></BaseStatusForm>
+      );
       break;
   }
   return form;
@@ -75,13 +122,11 @@ export default function StatusFormDemo() {
     (key) => typeof formStatus[key] !== "function"
   );
   return statusList.map((status) => (
-    <>
-      <section>
-        <h4>
-          form status <em>{status}</em>
-        </h4>
-        <StatusForm status={status}></StatusForm>
-      </section>
-    </>
+    <section key={status}>
+      <h4>
+        form status <em>{status}:</em>
+      </h4>
+      <StatusForm status={status}></StatusForm>
+    </section>
   ));
 }
