7a5a0e10058261c06035b0cbaec72095acf15109
xiaoRoy <xiaoroyg@163.com>
2025-01-02 23:26:36 +0800

    Finished challenge#4 - multiple selections inbox.
    Based on https://react.dev/learn/choosing-the-state-structure#challenges

--- src/learning-react/first/src/react-official/manage-state/state-structure/challenges/inbox.jsx
+++ src/learning-react/first/src/react-official/manage-state/state-structure/challenges/inbox.jsx
@@ -27,25 +27,51 @@ class Email {
   }
 }
 
+const KEY_SELECTED_EMAIL = "selected-email";
+
 const initialEmails = [
   Email.createEmail("Ready for adventure?", true),
   Email.createEmail("Time to check in!'"),
   Email.createEmail("Festival Begins in Just SEVEN Days!"),
 ];
 
-function EmailItem({ email, handleFavorite, handlerHover, isHighlighted }) {
+function EmailItem({
+  email,
+  handleFavorite,
+  handlerHover,
+  isHighlighted,
+  isMultiple,
+}) {
   const onFavoriteClicked = () => handleFavorite(email);
   const onEmailHovered = () => handlerHover(email);
   const hoveredStyleClassName = isHighlighted ? "highlighted" : "";
+  const emailSubject = email.subject;
 
+  const itemWithStar = (
+    <>
+      <button onClick={onFavoriteClicked}>{email.favoriteTitle}</button>
+      {emailSubject}
+    </>
+  );
+
+  const multipleSelectionItem = (
+    <label htmlFor={emailSubject}>
+      <input
+        type="checkbox"
+        id={emailSubject}
+        name={KEY_SELECTED_EMAIL}
+        value={email.id}
+      ></input>
+      {emailSubject}
+    </label>
+  );
   return (
     <li
       onMouseMove={onEmailHovered}
       className={hoveredStyleClassName}
       onFocus={onEmailHovered}
     >
-      <button onClick={onFavoriteClicked}>{email.favoriteTitle}</button>
-      {email.subject}
+      {isMultiple ? multipleSelectionItem : itemWithStar}
     </li>
   );
 }
@@ -53,6 +79,9 @@ function EmailItem({ email, handleFavorite, handlerHover, isHighlighted }) {
 function Inbox({ emails }) {
   const [emailList, setEmailList] = useState(emails);
   const [hoveredEmailId, setHoveredEmailId] = useState(null);
+  const [isMultiple, setMultiple] = useState(false);
+
+  const [selectedEmailIds, setSelectedEmailIds] = useState([]);
 
   const handleFavorite = (targetEmail) => {
     const updatedEmails = emailList.map((email) => {
@@ -72,10 +101,43 @@ function Inbox({ emails }) {
     setHoveredEmailId(targetEmail.id);
   };
 
+  const onMultipleSelectionClicked = () => {
+    setMultiple((value) => !value);
+    setSelectedEmailIds([]);
+  };
+
+  const onEmailSelected = (event) => {
+    if (event.target.name === KEY_SELECTED_EMAIL) {
+      const emailId = event.target.value;
+      const index = selectedEmailIds.indexOf(emailId);
+      let updatedEmailIds;
+      if (index != -1) {
+        updatedEmailIds = selectedEmailIds.filter((id) => id !== emailId);
+      } else {
+        updatedEmailIds = Array.from(selectedEmailIds);
+        updatedEmailIds.push(emailId);
+      }
+      setSelectedEmailIds(updatedEmailIds);
+    }
+  };
+
+  const selectedEmailsCount = selectedEmailIds.length;
+  const letterText = selectedEmailsCount == 1 ? "letter" : "letters";
+  const selectedEmailsText = `${selectedEmailsCount} ${letterText}`;
+
   return (
     <section>
-      <h2>Inbox</h2>
-      <ul>
+      <div className="inbox-title-container">
+        <h2>Inbox</h2>
+        <button
+          onClick={onMultipleSelectionClicked}
+          className="btn-multiple-selection"
+        >
+          Multiple Selection
+        </button>
+      </div>
+
+      <ul onChange={onEmailSelected}>
         {emailList.map((email) => (
           <EmailItem
             key={email.id}
@@ -83,9 +145,11 @@ function Inbox({ emails }) {
             handleFavorite={handleFavorite}
             handlerHover={handleHover}
             isHighlighted={hoveredEmailId === email.id}
+            isMultiple={isMultiple}
           ></EmailItem>
         ))}
       </ul>
+      {isMultiple && <p>You selected {selectedEmailsText}</p>}
     </section>
   );
 }
