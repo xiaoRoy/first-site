340fb6ce5c338a3321fd27eeecd1623fb063a677
xiaoRoy <xiaoroyg@163.com>
2025-02-18 15:47:23 +0800

    Learned dynamic loading.

---
+++ src/references/java-script/guide/java-script-modules/dynamic-loading/dynamic-loading.js
@@ -0,0 +1,32 @@
+import * as Canvas from "../basic-modules/canvas.js";
+
+const btnCircle = document.querySelector(".circle");
+const btnTriangle = document.querySelector(".triangle");
+const btnSquare = document.querySelector(".square");
+
+const canvas = Canvas.create("canvas", document.body, 480, 320);
+const listId = Canvas.createReportList(canvas.id);
+
+btnCircle.addEventListener("click", () => {
+  import("../basic-modules/circle.js").then((Circle) => {
+    const circle = Circle.draw(canvas.context, 50, 50, 100, "blue");
+    Circle.reportArea(circle.radius, listId);
+    Circle.reportPerimeter(circle.radius, listId);
+  });
+});
+
+btnTriangle.addEventListener("click", () => {
+  import("../basic-modules/triangle.js").then((Triangle) => {
+    const triangle = Triangle.draw(canvas.context, 100, 75, 190, "yellow");
+    Triangle.reportArea(triangle.length, listId);
+    Triangle.reportPerimeter(triangle.length, listId);
+  });
+});
+
+btnSquare.addEventListener("click", () => {
+  import("../basic-modules/square.js").then((Square) => {
+    const square = Square.draw(canvas.context, 75, 200, 100, "green");
+    Square.reportArea(square.length, listId);
+    Square.reportPerimeter(square.length, listId);
+  });
+});
